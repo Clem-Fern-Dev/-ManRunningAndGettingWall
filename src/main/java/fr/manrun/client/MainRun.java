49ef7bbab72e4317eb788da33bb3a56bc2b7d000
Eteckq <yohan.g99@gmail.com>
2018-02-14 20:19:21 +0100

    Yohan

--- src/main/java/fr/manrun/client/MainRun.java
+++ src/main/java/fr/manrun/client/MainRun.java
@@ -6,32 +6,6 @@ import com.jogamp.opengl.GLProfile;
 
 public class MainRun {
 	
-	private static GLWindow win = null;
-		
-	public static void init() {
-		GLProfile.initSingleton();
-		GLProfile glProfile = GLProfile.get(GLProfile.GL2);
-		
-		GLCapabilities caps = new GLCapabilities(glProfile);
-		
-		win = GLWindow.create(caps);
-		win.setSize(1280, 720);
-		win.setResizable(false);
-		
-		win.setVisible(true);
-		
-	}
-	
-	public static void main(String[] args) {
-		init();
-	}
 
-	public static GLWindow getWin() {
-		return win;
-	}
-
-	public static void setWin(GLWindow win) {
-		MainRun.win = win;
-	}
 
 }
